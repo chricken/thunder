a263f1ea329f69e93138b075782dd050746d8bf1
C. Heisch <chrickenh@gmail.com>
2024-03-06 08:38:28 +0100

    Kleinere Änderungen, Blitz kann sich bald verästeln

--- fe/assets/js/classes/Lightning.js
+++ fe/assets/js/classes/Lightning.js
@@ -4,39 +4,50 @@ import settings, { elements } from '../settings.js';
 
 class Lightning {
     constructor() {
-        this.start = {
-            x: Math.random(),
-            y: 0,
-            angle: Math.random() * (.5 * Math.PI) - (.25 * Math.PI)
-        };
         this.minStepWidth = .05;
         this.maxStepWidth = .1;
         this.maxDeltaAngle = .2 * Math.PI;
-        this.path = [];
+        this.paths = [[{
+            x: Math.random(),
+            y: 0,
+            angle: Math.random() * (.5 * Math.PI) - (.25 * Math.PI)
+        }]];
+        this.probabilityToBranch = .1;
 
-        this.addPart(this.start);
+        this.addPart();
     }
-    addPart(lastPoint) {
-        let distance = Math.random() * (this.maxStepWidth - this.minStepWidth) + this.minStepWidth;
-        let angle = lastPoint.angle + (Math.random() * (this.maxDeltaAngle * 2) - this.maxDeltaAngle);
+    addPart() {
+        this.paths.forEach(path => {
+            let lastPoint = path[path.length - 1];
+            let distance = Math.random() * (this.maxStepWidth - this.minStepWidth) + this.minStepWidth;
+            let angle = lastPoint.angle + (Math.random() * (this.maxDeltaAngle * 2) - this.maxDeltaAngle);
 
-        // clamp
-        angle = Math.max(angle, -Math.PI / 2);
-        angle = Math.min(angle, Math.PI / 2);
-        console.log(angle);
-        let deltaX = Math.sin(angle) * distance;
-        let deltaY = Math.cos(angle) * distance;
+            // clamp
+            angle = Math.max(angle, -Math.PI / 2);
+            angle = Math.min(angle, Math.PI / 2);
+            let deltaX = Math.sin(angle) * distance;
+            let deltaY = Math.cos(angle) * distance;
+            
+            console.log(lastPoint.y + deltaY);
+            let point = {
+                x: lastPoint.x + deltaX,
+                y: lastPoint.y + deltaY,
+                angle
+            }
+            path.push(point);
 
-        let point = {
-            x: lastPoint.x + deltaX,
-            y: lastPoint.y + deltaY,
-            angle
-        }
-        this.path.push(point)
+            if (Math.random() < this.probabilityToBranch) {
 
-        // Eintragen des nächsten Blitzes
-        if (point.y <= 1) {
-            setTimeout(this.addPart.bind(this), 60, point)
+            }
+
+        })
+
+        // Eintragen des nächsten Punktes
+        let lowest = this.paths.reduce((max, path) => {
+            return Math.max(max, path[path.length-1].y)
+        }, 0)
+        if(lowest <= 1){
+            setTimeout(this.addPart.bind(this), 100)
         }
     }
     update() {
@@ -45,24 +56,26 @@ class Lightning {
     render() {
         const c = elements.spielfeld;
         const ctx = c.getContext('2d');
+        this.paths.forEach(path => {
 
-        // Lightning
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = '#f00';
+            // Lightning
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = '#f00';
 
-        ctx.beginPath();
-        ctx.moveTo(
-            this.start.x * c.width,
-            this.start.y * c.height
-        )
-
-        this.path.forEach(point => {
-            ctx.lineTo(
-                point.x * c.width,
-                point.y * c.height
+            ctx.beginPath();
+            ctx.moveTo(
+                path[0].x * c.width,
+                path[0].y * c.height
             )
+
+            path.forEach(point => {
+                ctx.lineTo(
+                    point.x * c.width,
+                    point.y * c.height
+                )
+            })
+            ctx.stroke();
         })
-        ctx.stroke();
     }
     checkBorders() {
 
